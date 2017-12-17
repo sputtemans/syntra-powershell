4a71677c4e5bff22fc59e0823fc835f2331220ba
sputtemans <34538613+sputtemans@users.noreply.github.com>
2017-12-17 16:49:29 +0100

    Add files via upload
    Script om users in AD in te voeren via csv file

---
+++ Import-Users-AD.ps1
@@ -0,0 +1,17 @@
+﻿Param (
+    [Parameter(Mandatory=$true)][string]$CsvLocatie
+    )
+ 
+
+$Users = Import-Csv -Path $CsvLocatie -Delimiter ";"           
+foreach ($User in $Users)            
+{            
+    $Displayname = $User.Accountnaam           
+    $UserFirstname = $User.Voornaam            
+    $UserLastname = $User.Achternaam  
+    $Password = $User.Wachtwoord             
+    $OU = $User.'OU'                     
+    $Servername = $User.Servernaam          
+
+    New-ADUser -Name "$Displayname" -DisplayName "$Displayname" -GivenName "$UserFirstname" -Surname "$UserLastname" -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -Path "$OU" -ChangePasswordAtLogon $false -server $Servername
+}
