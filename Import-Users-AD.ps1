753d93f39585143dca46c803d3a88ac71454975a
sputtemans <34538613+sputtemans@users.noreply.github.com>
2018-01-07 15:08:21 +0100

    Add files via upload

---
+++ Import-Users-AD.ps1
@@ -0,0 +1,16 @@
+﻿Param (
+    [Parameter(Mandatory=$true)][string]$CsvLocatie
+    )
+ 
+
+$Users = Import-Csv -Path $CsvLocatie -Delimiter ";"           
+foreach ($User in $Users)            
+{            
+    $Displayname = $User.Accountnaam           
+    $UserFirstname = $User.Voornaam            
+    $UserLastname = $User.Achternaam  
+    $Password = $User.Wachtwoord                                
+    $Servername = $User.Servernaam          
+
+    New-ADUser -Name "$Displayname" -DisplayName "$Displayname" -GivenName "$UserFirstname" -Surname "$UserLastname" -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -ChangePasswordAtLogon $false
+}
